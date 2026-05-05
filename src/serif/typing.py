8af56ca83d88a30bee602176fc86a3784a1e7239
TheAerius <ryan.e.larsen@gmail.com>
2026-05-05 09:33:56 -0400

    remove unreferenced files

--- src/serif/typing.py
+++
@@ -1,9 +0,0 @@
-# Re-export from canonical location for backward compatibility.
-# DataType and friends now live in serif._vector.dtype
-from ._vector.dtype import DataType
-from ._vector.dtype import infer_dtype
-from ._vector.dtype import infer_kind
-from ._vector.dtype import validate_scalar
-
-__all__ = ["DataType", "infer_dtype", "infer_kind", "validate_scalar"]
-
